a4ba299c1fabaa42bbf3a5406652f964479133b8
Alexandru Contu <alexandru.contu99@gmail.com>
2023-04-20 20:47:42 +0300

    modify the file

---
+++ src/test/java/com/unibuc/ismyblog/service/impl/ContactInfoServiceImplTest.java
@@ -0,0 +1,11 @@
+package com.unibuc.ismyblog.service.impl;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ContactInfoServiceImplTest {
+  
+}
