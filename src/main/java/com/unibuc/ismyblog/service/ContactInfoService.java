fbc3f787079a5702e67cdd9aeb722432f557822a
Alexandru Contu <alexandru.contu99@gmail.com>
2023-04-23 21:15:37 +0300

    add user update and contact info update

--- src/main/java/com/unibuc/ismyblog/service/ContactInfoService.java
+++ src/main/java/com/unibuc/ismyblog/service/ContactInfoService.java
@@ -4,4 +4,6 @@ import com.unibuc.ismyblog.model.ContactInfo;
 
 public interface ContactInfoService {
     ContactInfo save(ContactInfo contactInfo);
+    ContactInfo findById(Long contactInfoId);
+    void deleteContactInfo(Long contactInfoId);
 }
